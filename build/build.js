af3451f3b8944227e8ddce9bacd37c264c638f31
dinony <contact@dinony.com>
2017-07-31 13:13:26 +0200

    Prepare eslint, babel and rollup. Fix eslint warnings

---
+++ build/build.js
@@ -0,0 +1,8 @@
+
+
+const builds = [
+  {dest: 'dist/mo-query.js', format: 'umd', env: 'development'},
+  {dest: 'dist/mo-query.js', format: 'umd', env: 'production'},
+  {dest: 'dist/mo-query.cjs.js', format: 'cjs'},
+  {dest: 'dist/mo-query.es.js', format: 'es'}
+];
