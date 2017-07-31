26789382e4b948c51fe150ede6042a4efb3cfd6a
dinony <contact@dinony.com>
2017-07-31 16:02:08 +0200

    Prepare build script

--- build/build.js
+++ build/build.js
@@ -1,8 +1,62 @@
+// Inspired by: https://github.com/vuejs/vue-router/blob/dev/build/build.js
+const path = require('path')
+const fs = require('fs')
+const nodeResolve = require('rollup-plugin-node-resolve')
+const uglify = require('uglify-js')
+const cjs = require('rollup-plugin-commonjs')
+const babel = require('rollup-plugin-babel')
+const rollup = require('rollup')
+const zlib = require('zlib')
+const rimraf = require('rimraf')
+const prettyBytes = require('pretty-bytes')
 
+rimraf.sync('dist')
+fs.mkdirSync('dist')
 
-const builds = [
-  {dest: 'dist/mo-query.js', format: 'umd', env: 'development'},
-  {dest: 'dist/mo-query.js', format: 'umd', env: 'production'},
-  {dest: 'dist/mo-query.cjs.js', format: 'cjs'},
-  {dest: 'dist/mo-query.es.js', format: 'es'}
-];
+const isProd = filename => /min\.js$/.test(filename)
+const resolve = _path => path.resolve(__dirname, '../', _path)
+const relPath = _path => path.relative(process.cwd(), _path)
+
+const writeFile = (dest, code) =>
+  (new Promise((resolve, reject) => {
+    fs.writeFile(dest, code, err => {
+      if(err) return reject(err)
+
+      return resolve(code)
+    })
+  }))
+
+const uglifyConf = {output: {ascii_only: true}};
+
+[
+  {dest: resolve('dist/mo-query.js'), format: 'umd'},
+  {dest: resolve('dist/mo-query.min.js'), format: 'umd'},
+  {dest: resolve('dist/mo-query.cjs.js'), format: 'cjs'},
+  {dest: resolve('dist/mo-query.esm.js'), format: 'es'}
+].map(({dest, format}) => ({
+  entry: resolve('src/query.js'), dest, format, moduleName: 'moQuery',
+  plugins: [
+    nodeResolve(),
+    cjs(),
+    babel({exclude: 'node_modules/**'})
+  ]
+})).forEach(c => {
+  rollup.rollup(c).then(bundle => bundle.generate(c))
+    .then(({code}) => {
+      if(isProd(c.dest)) {
+        const uglified = uglify.minify(code, uglifyConf).code
+        writeFile(c.dest, uglified, true)
+
+        const zipped = zlib.gzipSync(uglified)
+
+        // eslint-disable-next-line no-console
+        console.log(`${relPath(c.dest)} ${prettyBytes(uglified.length)} (gzipped: ${prettyBytes(zipped.length)})`)
+      } else {
+        writeFile(c.dest, code)
+
+        // eslint-disable-next-line no-console
+        console.log(`${relPath(c.dest)} ${prettyBytes(code.length)}`)
+
+      }
+    }).catch(console.log) // eslint-disable-line no-console
+})
